8ee8863dbb79a45bb88ca05652562ed3b8555221
GeorgeDeyneka <zhoradeynecka@gmail.com>
2023-02-01 20:13:07 +0200

    Update app, add base template, form group & form control

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,19 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss'],
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent implements OnInit, OnDestroy {
   public form: FormGroup;
+  public formSubj$: Subscription;
 
   constructor(private fb: FormBuilder) {}
 
+  get password() {
+    return this.form.get('password');
+  }
+
   ngOnInit(): void {
     this.form = this.fb.group({
-      password: '',
+      password: ['', [Validators.required, Validators.minLength(8)]],
     });
+
+    this.formSubj$ = this.form.valueChanges.subscribe((value) =>
+      console.log(value.password)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.formSubj$.unsubscribe();
   }
 }
